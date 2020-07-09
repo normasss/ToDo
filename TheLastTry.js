c8d2bd8b09361790466aad66412c3091fd6072f2
normasss <normasss@yandex.ru>
2020-07-09 18:15:46 +0300

    мефедрон2

--- TheLastTry.js
+++ TheLastTry.js
@@ -1,20 +1,47 @@
 $( document ).ready(function() {
 
-var text;
+  let arrOfItems = []
+  const enterCode = 13
 
-  $('#addBtn').on('click', function () {
-  text = $('#textField').val().trim()
-    console.log(text)
+  $('#add-btn').on('click', function () {
+    let itemList = {
+      textOfItem: $('#text-field').val().trim() ,
+      idOfItem: Math.random(),
+      copmleted: false
+    }
+    arrOfItems.push(itemList)
+    $('#text-field').val('')
+    render()
   })
 
-  $('#textField').on('keypress',function(e) {
-    if(e.which == 13) {
-      text = $('#textField').val().trim()
-      console.log(text)
+
+  $('#text-field').on('keypress',function(e) {
+    if(e.which == enterCode) {
+      render()
     }
   })
 
 
+  let render = () => {
+    let textString = '';
+    arrOfItems.forEach(item => {
+      textString = `<li id="${item.idOfItem}" >
+        <input type="checkbox" class="toDoDone">
+        <span class='${ item.copmleted ? 'completed' : ''}' >${item.textOfItem}</span>
+    </li>`
+    })
+    $('#listToDo').append(textString)
+  }
+
+  $(document).on('change','.toDoDone', function() {
+    let newId = $(this).parent().attr('id')
+    arrOfItems.forEach(item => {
+      if (Number(item.idOfItem) === Number(newId)) {
+        item.copmleted = !item.copmleted
+      }
+    })
+    render()
+  })
 
 
 
---
+++ learninig.js
@@ -0,0 +1,244 @@
+
+
+
+
+
+
+// $( document ).ready(function() {
+//   $('#1').on('click', () => {
+//     alert($('#2').val())
+//   })
+//
+// });
+
+// let name = {
+//   name: 'anantoly',
+//   surname: 'vasserman',
+//   zalupa: 'big',
+//   penis: 'small',
+//   brain: 'veryBig',
+// }
+// Object.keys(name).forEach((item)=>{
+//   console.log(item)
+//   console.log(name[item])
+// })
+// $( document ).ready(function() {
+//
+//
+//
+//
+//
+//
+//
+//   let arr = []
+//
+//
+//   $( '#1').on( "click", function() {
+//     let a = $('#2').val()
+//     //console.log(a)
+//     if (a.valueOf().charAt(0) == ' ' || a.valueOf().charAt(0) == '')
+//     {
+//       a = ''
+//       console.log('empty')
+//     }
+//     else
+//     {
+//       console.log('not empty')
+//
+//       //$('#2').val()
+//       $('<ul><li></li></ul>').text(a).appendTo(document.ul)
+//       checkBox = $('<input type="checkbox"></input>')
+//         .appendTo(document.body)
+//       let deleteButton = $('<input type="button" value="Delete"></input>')
+//         .appendTo(document.body)
+//       let x = $(document.createElement("input"))
+//
+//       //console.log(a)
+//       arr.push(a)
+//       console.log(arr)
+//       $('#2').val(' ')
+//     }
+//   });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// });
+
+
+$( document ).ready(function() {
+
+  let arr = []
+
+
+
+  $('#addButton').on( 'click', function () {
+    let a = $('#inputField').val().trim();
+    if(a !== '') {
+      createObject(a);
+    } else {
+      console.log('empty')
+    }
+  } )
+
+
+  function createObject(a) {
+    let taskObject = {
+      text: a,
+      id: Math.random(),
+      checked: false
+    }
+    arr.push(taskObject)
+    renderTask();
+  }
+
+
+  function renderTask() {
+    let forRender = ''
+    arr.forEach((item) => {
+      forRender += `
+      <li id= '${item.id}'>
+      <input type="checkbox" class="checked" ${item.checked ? 'checked' : ''}>
+      <span class="${item.checked ? 'activate' : ''}" >${item.text}</span>
+      <input type="button" value="delete" class="zalupa">
+      </li>
+      `
+    })
+    $('#list').html(forRender);
+    $('#inputField').val('');
+  }
+
+$(document).on('click', '.zalupa', function () {
+  const parentId = $(this).parent().attr('id')
+  arr.forEach((item, index) => {
+    if (Number(item.id) == Number(parentId)){
+      arr.splice(index, 1)
+    }
+  })
+  renderTask();
+
+
+})
+
+
+  $(document).on('change','.checked', function(){
+    const parentId = $(this).parent().attr('id')
+    arr.forEach((item) => {
+     if (Number(item.id) == Number(parentId) ){
+       item.checked = !item.checked;
+     }
+    })
+    renderTask()
+    console.log(arr)
+  })
+
+
+  $(document).on('keypress', function(event) {
+    if(event.which === 13) {
+      let a = $('#inputField').val().trim();
+      if(a !== '') {
+        createObject(a);
+      } else {
+        console.log('empty')
+      }
+    }
+  });
+
+
+  $('#selectAll').on('change', function(){
+    arr.forEach((item) => {
+      if (Number(item.id)){
+        item.checked = !item.checked;
+      }
+    })
+    renderTask()
+  })
+
+
+  $('#deleteButton').on('click', function () {
+    let vladislav = arr.filter( item => item.checked === false)
+    arr = vladislav
+    console.log(arr)
+    renderTask();
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
