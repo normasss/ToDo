0fe0f29820962c725f98a0b2fbfedca922c92806
normasss <normasss@yandex.ru>
2020-07-10 22:25:40 +0300

    mef4ik

--- TheLastTry.js
+++ TheLastTry.js
@@ -28,11 +28,28 @@ $( document ).ready(function() {
       textString += `<li id="${item.idOfItem}" >
         <input type="checkbox" class="toDoDone" ${ item.copmleted ? 'checked' : ''}>
         <span class='${ item.copmleted ? 'completed' : ''}' >${item.textOfItem}</span>
+        <input type="button" class="delete-btn">
     </li>`
     })
     $('#listToDo').html(textString)
     checkBoxFunction()
   }
+
+  $(document).on('click','.delete-btn', function() {
+    let deleteItem = $(this).parent().attr('id')
+    arrOfItems.forEach((item, index) => {
+      if (Number(item.idOfItem) === Number(deleteItem)) {
+        // удаляет все  let indexToDelete = arrOfItems.filter(item => item.idOfItem)
+          arrOfItems.splice(index, 1)
+      }
+    })
+    render()
+  })
+
+  $('.delete-all').on('click', function() {
+        arrOfItems.length = 0;
+      render()
+    })
 
   $(document).on('change','.toDoDone', function() {
     let newId = $(this).parent().attr('id')
@@ -65,7 +82,17 @@ $( document ).ready(function() {
     }
   }
 
+  let sortedArr = [];
+    $('.delete-checked').on('click', function() {
+      let idOfCheckedItems = $(this).parent().attr('id')
 
+      arrOfItems.forEach((item, index)=> {
+        sortedArr = arrOfItems.filter(item => item.copmleted !== true)
+      })
+      arrOfItems = sortedArr
+      console.log(sortedArr)
+      render()
+    })
 
 
 
