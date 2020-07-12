cd7faac0c86cda32adf79ecb140f8e736659519b
normasss <normasss@yandex.ru>
2020-07-12 16:59:39 +0300

    mefendron4ik

--- TheLastTry.js
+++ TheLastTry.js
@@ -3,35 +3,49 @@ $( document ).ready(function() {
   let arrOfItems = []
   const enterCode = 13
 
-  $('#add-btn').on('click', function () {
+   $('#add-btn').on('click', function () {
     let itemList = {
       textOfItem: $('#text-field').val().trim() ,
       idOfItem: Math.random(),
-      copmleted: false
+      copmleted: false,
+      onEdit: false
     }
+    arrOfItems.forEach(item => item.onEdit = false)
     arrOfItems.push(itemList)
     $('#text-field').val('')
     render()
   })
 
 
-  $('#text-field').on('keypress',function(e) {
-    if(e.which == enterCode) {
+  $('#text-field').on('keydown',function(event) {
+    if( Number(event.keyCode) === Number(enterCode)) {
+      let itemList = {
+        textOfItem: $('#text-field').val().trim() ,
+        idOfItem: Math.random(),
+        copmleted: false,
+        onEdit: false
+      }
+      arrOfItems.forEach(item => item.onEdit = false)
+      arrOfItems.push(itemList)
+      $('#text-field').val('')
       render()
     }
   })
-
 
   let render = () => {
     let textString = '';
     arrOfItems.forEach(item => {
       textString += `<li id="${item.idOfItem}" >
         <input type="checkbox" class="toDoDone" ${ item.copmleted ? 'checked' : ''}>
-        <span class='${ item.copmleted ? 'completed' : ''}' >${item.textOfItem}</span>
-        <input type="button" class="delete-btn">
+        <span class='${ item.copmleted ? 'completed' : ''}' >${ item.onEdit ? '<input type="text" class="new-value">' : item.textOfItem}</span>
+        ${ item.onEdit ? '<input type="button" value="Сохранить изменения" class="save-changes">' : '<input type="button" class="edit-btn" value="Редактировать">'}
+        <input type="button" class="delete-btn" value="Удалить">
     </li>`
     })
     $('#listToDo').html(textString)
+    $('.all-checkboxes').html('Всего задач:' + allItemsCount())
+    $('.all-checked-checkboxes').html('Выполненных задач:' + сheckedItemsCount())
+    $('.all-unchecked-сheckboxes').html('Невыполненных задач:' + uncheckedItemsCount())
     checkBoxFunction()
   }
 
@@ -90,12 +104,37 @@ $( document ).ready(function() {
         sortedArr = arrOfItems.filter(item => item.copmleted !== true)
       })
       arrOfItems = sortedArr
-      console.log(sortedArr)
       render()
     })
 
+    $(document).on('click', '.edit-btn' , function () {
+      let editItem = $(this).parent().attr('id')
+      arrOfItems.forEach(item => {
+        if (Number(item.idOfItem) === Number(editItem)){
+          item.onEdit = true
+        } else {
+          item.onEdit = false
+        }
+      })
+      render()
+    })
 
+  let newTextOfItem = '';
+  $(document).on('click', '.save-changes' , function () {
+    let editItem = $(this).parent().attr('id')
+    arrOfItems.forEach(item => {
+      if (Number(item.idOfItem) === Number(editItem)){
+        newTextOfItem = $('.new-value').val().trim()
+        item.textOfItem = newTextOfItem ? newTextOfItem : item.textOfItem
+        item.onEdit = false
+      }
+    })
+    render()
+  })
 
+  let uncheckedItemsCount = () => arrOfItems.filter(item => item.copmleted == false).length
+  let сheckedItemsCount = () => arrOfItems.filter(item => item.copmleted == true).length
+  let allItemsCount = () => arrOfItems.length
 
 
 
