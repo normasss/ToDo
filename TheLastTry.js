c7fae5e5b39fb2a50c13acf36c162bc64456c930
normasss <normasss@yandex.ru>
2020-07-18 00:18:40 +0300

    pagination

--- TheLastTry.js
+++ TheLastTry.js
@@ -1,18 +1,17 @@
 $( document ).ready(function() {
 
+
+
   let arrOfItems = [];
   let currentTab = 'all';
-  const enterCode = 13
+  const enterCode = 13;
 
    $('#add-btn').on('click', function () {
     let itemList = {
       textOfItem: $('#text-field').val().trim() ,
       idOfItem: Math.random(),
       copmleted: false,
-      onEdit: false,
-      tab3: true,
-      tab2: false,
-      tab2: false
+      onEdit: false
     }
     arrOfItems.forEach(item => item.onEdit = false)
     arrOfItems.push(itemList)
@@ -20,6 +19,9 @@ $( document ).ready(function() {
     render()
   })
 
+$('#show-all').on('click', function () {
+  $('#all').append(arrOfItems.length)
+})
 
   $('#text-field').on('keydown',function(event) {
     if( Number(event.keyCode) === Number(enterCode)) {
@@ -33,6 +35,9 @@ $( document ).ready(function() {
       arrOfItems.push(itemList)
       $('#text-field').val('')
       render()
+      //pageAddFunction()
+      //renderPages()
+      pagesFunction()
     }
   })
   const activeTab = function(str) {
@@ -77,10 +82,27 @@ $( document ).ready(function() {
 
     return currentList;
   };
-  
+  // const maxElementsOnPage = 5;
+  // let currentPage = 1;
+  // let pagination = (currentList) => {
+  // let lastPage = Math.ceil(currentList.length / maxElementsOnPage)
+  //   if (currentPage > maxElementsOnPage) {
+  //
+  //     lastPage = currentPage
+  //
+  //   }
+  //   $('#pages').html('')
+  //   currentPage = currentPage === 0 ? 1 : currentPage
+  //   for(i=0; i<lastPage+1; i++) {
+  //     const el = $(`<button>${i}</button>`)
+  //     $('#pages').append(el)
+  //   }
+  // }
+
 
   let render = () => {
     let currentList = todoTab();
+    //pagination(currentList)
     let textString = '';
     currentList.forEach(item => {
       textString += `<li id="${item.idOfItem}" >
@@ -184,5 +206,128 @@ $( document ).ready(function() {
     render()
   })
 
+  // let firstPage = 1;
+  // let secondPage = 2;
+  // let arrOfPages = [];
+  // let arrOfObjectOfPages = [];
+  // let pageAddFunction = () => {
+  //   let lengthOfArrOfItems = Number(arrOfItems.length);
+  //   let multiplicity = 10;
+  //   startSizeOfArrOfPages = []
+  //   let result =  Number(lengthOfArrOfItems) % Number(multiplicity);
+  //   if (lengthOfArrOfItems == 11) {
+  //     let newPage = {
+  //       numberOfPage: lengthOfArrOfItems / multiplicity,
+  //       id: Math.random()
+  //     }
+  //     arrOfPages.push(newPage.numberOfPage = firstPage)
+  //     arrOfObjectOfPages.push(newPage)
+  //     //renderPages()
+  //     //arrOfPages.push(newPage.numberOfPage = 2)
+  //     //arrOfObjectOfPages.push(newPage)
+  //     console.log(arrOfObjectOfPages)
+  //     let var1 = ''
+  //     arrOfPages.forEach(item => {
+  //       var1 =`<button id="${item.id}">${Math.floor(lengthOfArrOfItems / multiplicity)}</button>`
+  //     })
+  //     $('#pages').append(var1)
+  //     //renderPages()
+  //
+  //
+  //   } else if (result == 1 && lengthOfArrOfItems > 11){
+  //     let newPage = {
+  //       numberOfPage: Math.floor(lengthOfArrOfItems / multiplicity),
+  //       id: Math.random()
+  //     }
+  //     //let numOfPage = Math.floor(15.5)//(Number(lengthOfArrOfItems) / Number(multiplicity))
+  //     lengthOfArrOfItems += 9
+  //     //arrOfPages.push(numOfPage)
+  //     lengthOfArrOfItems -= 9
+  //     arrOfObjectOfPages.push(newPage)
+  //     //$('#pages').append($(`<button >${Math.floor(lengthOfArrOfItems / multiplicity)}</button>`))
+  //     arrOfPages.push(newPage.numberOfPage)
+  //     console.log(arrOfObjectOfPages)
+  //     console.log('arrOfPages', arrOfPages)
+  //     let var1 = ''
+  //     arrOfPages.forEach(item => {
+  //       var1 =`<button id="${item.id}">${Math.floor(lengthOfArrOfItems / multiplicity)}</button>`
+  //     })
+  //     $('#pages').append(var1)
+  //
+  //   } else if (arrOfPages.length == startSizeOfArrOfPages.length ) {
+  //
+  //     //renderPages()
+  //
+  //   }
+  //   }
 
-  }); // refers to document.ready
+  // $(document).on('click', '.current-page', function () {
+  //   console.log(1)
+  //   $('#pages li').removeClass('active')
+  //   $(this).addClass('active')
+  // })
+
+  let pagesFunction = () => {
+    let numberOfItems = arrOfItems.length;
+    let limitPerPage = 6;
+    let totalPages = Math.round(numberOfItems / limitPerPage)
+    let delitel = numberOfItems % limitPerPage
+    if (numberOfItems == limitPerPage ) {
+      $("#pages").append("<li class='current-page '><a href='#'>" + 1 + "</a></li>")
+    }
+
+    else if (delitel === 0) {
+      for (let i=totalPages; i <= totalPages; i++){
+        $("#pages").append("<li class='current-page'><a href='#'>" + i + "</a></li>");
+      }
+    }
+
+    $(document).on('click', '.current-page', function () {
+      $('#pages li').removeClass('active')
+      $(this).addClass('active')
+
+      if ($(this).hasClass('active')){
+        let numOfPage = $(this).text()
+        $("#listToDo li").hide()
+        let firstElementIndex = numOfPage * (limitPerPage - 1) - (limitPerPage - 1)
+        for (let i = firstElementIndex; i < (numOfPage * (limitPerPage - 1)); i++ ) {
+          $('#listToDo li:eq(' + i + ')').show()
+        }
+      }
+
+    })
+
+
+
+  }
+
+
+
+  // let one = $("#listToDo").find(`li`).show(arrOfItems.splice(limitPerPage - 1))
+  // $("#pages").append("<li class='current-page active'><a href='javascript:void(0)'>" + 1 + "</a></li>");
+  //
+  //
+  // for (let i = 2; i <= totalPages; i++) {
+  //   $("#pages").append("<li class='current-page'><a href='javascript:void(0)'>" + i + "</a></li>");
+  // }
+
+
+
+
+
+
+
+    // let renderPages = () => {
+    // // let var1 = ''
+    // // let appendedValue = arrOfPages.forEach(item => var1 += $(`<button id="${item.id}">${item.numberOfPage}</button>`))
+    // //   $('#pages').append(appendedValue)
+    //   let var1 = ''
+    //   arrOfPages.forEach(item => {
+    //     var1 +=`<button id="${item.id}">${Math.floor(lengthOfArrOfItems / multiplicity)}</button>`
+    //   })
+    //   $('#pages').append(var1)
+    //   //$('#pages').append($(`<button >${Math.floor(lengthOfArrOfItems / multiplicity)}</button>`))
+//}
+
+  //arrOfItems.slice(item.idOfItem)
+}); // refers to document.ready
